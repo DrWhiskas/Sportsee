df3af7757d62ec656e999c116ec8257f4a44c541
unknown <kayv@hotmail.fr>
2023-10-28 12:14:30 +0200

    Update API.js

--- src/api/API.js
+++ src/api/API.js
@@ -8,7 +8,7 @@ import {
 
 // requete API
 export default async function API(id) {
-	const APIMock = true;
+	const APIMock = false;
 	if (APIMock === false) {
 		let APIresponse; // variable qui stocke la reponse de l'API
 		let ApiActivityRes;
